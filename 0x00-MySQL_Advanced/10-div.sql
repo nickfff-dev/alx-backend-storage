50158d9ecaaeb681368a23911f5814d3185d62a4
nickfff-dev <hunchographics@gmail.com>
2024-03-24 02:36:57 +0300

    safe div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,12 @@
 -- Creates a function 'SafeDiv' that safely divides two numbers, returning 0 if the divisor is 0.
-
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER //
-
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 6)
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT DETERMINISTIC
 BEGIN
     IF b = 0 THEN
         RETURN 0;
     ELSE
         RETURN a / b;
     END IF;
-END; //
-
+END//
 DELIMITER ;
