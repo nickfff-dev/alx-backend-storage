7e9e76235ffc1b7dec9cf181be6d9a07c385515d
nickfff-dev <hunchographics@gmail.com>
2024-03-24 02:04:43 +0300

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- statement to order bands by lifespan
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE FIND_IN_SET("Glam rock", style) > 0 ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan FROM metal_bands WHERE FIND_IN_SET("Glam rock", style) > 0 ORDER BY lifespan DESC;
