ef4f00841b222d2aa0fd84d993e48dac6dfeca10
nickfff-dev <hunchographics@gmail.com>
2024-03-24 02:30:12 +0300

    names

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- Creates an index 'idx_name_first_score' on 'names' table for efficient queries filtering by the first letter of 'name' and 'score'.
-CREATE INDEX idx_name_first_score ON names (name(1)), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
