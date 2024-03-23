f6a469e1264153152884727c0943168a59188e61
nickfff-dev <hunchographics@gmail.com>
2024-03-24 01:01:47 +0300

    cleaned

--- 1-country_users.sql
+++
@@ -1,7 +0,0 @@
--- ddl operation to create table users with country enum
-CREATE TABLE IF NOT EXISTS users(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255),
-country ENUM('US', 'CO', 'TN') NOT NULL
-);
