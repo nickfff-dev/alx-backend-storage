414969180eac234b35e5d110475c3282f43289a1
nickfff-dev <hunchographics@gmail.com>
2024-03-24 02:40:25 +0300

    computed weighted average

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- Compute the average weighted score for the specified user
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+
+    UPDATE users
+    SET average_score = (
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END//
+
+DELIMITER ;
